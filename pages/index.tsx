1637265216225be8c6fa68cb1c47c2c1cc259e61
Darshan Satya <darshans1989@gmail.com>
2022-08-16 09:08:25 -0400

    gradient layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import GradientLayout from "../components/gradientLayout";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <GradientLayout color="blue" roundImage>
+      <div>Home Page</div>
+    </GradientLayout>
+  );
 }

5b41a01c2e3981b4d07aacd511fb71f2b66e7a27
Darshan Satya <darshans1989@gmail.com>
2022-08-18 08:43:22 -0400

    top artists

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,50 @@
-import Head from "next/head";
-import Image from "next/image";
+import { Flex, Text, Box } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
-import styles from "../styles/Home.module.css";
+import prisma from "../lib/prisma";
 
-export default function Home() {
+export default function Home({ artists }) {
   return (
     <GradientLayout
       roundImage
-      color="green"
+      color="purple"
       subtitle="profile"
       title="Darshan Satya"
       description="Is public playlist"
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
     >
-      <div>Home Page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">Only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="15%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="x-large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
 }
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+  console.log(artists);
+  return {
+    props: { artists },
+  };
+};
